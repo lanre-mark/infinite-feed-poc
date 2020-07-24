1d8c12f90c26b8c51a743fbc479ca8ef3438fca8
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-24 18:52:03 +0100

    initial graphql and next.js setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,6 @@ export default function Home() {
       <h1>Hello there!</h1>
       <p>Welcome to  platform 🤷</p>
       <span>Check out these pages:</span>
-      <ul>
-        <li>Project <Link href="/projects/10">Blue Onion Labs</Link></li>
-        <li>User <Link href="/users/11">Cai Burris</Link></li>
-     </ul>
     </>
   )
 }
