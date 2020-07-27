8aa0c47bb5261af363e9e4f5651e89ce92d061fb
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-27 21:55:24 +0100

    connected components, next app with queries

--- graphql/resolvers/Query/index.ts
+++ graphql/resolvers/Query/index.ts
@@ -1,5 +1,9 @@
 import _announcements from './announcements';
 import _feeds from './feeds';
+import _project from './project'
+import _user from './user'
 
 export const announcements = _announcements;
 export const feeds = _feeds;
+export const project = _project
+export const user = _user
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import FeedSlot from 'components/FeedSlot'
+import Layout from 'components/Layout'
+
 
 export default function Home() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>Infinite Newsfeed POC</title>
       </Head>
       <h1>Hello there!</h1>
       <p>Welcome to  platform 🤷</p>
       <span>Check out these pages:</span>
-    </>
+      <ul>
+        <li>Project <Link href="/projects/10">Blue Onion Labs</Link></li>
+        <li>User <Link href="/users/11">Cai Burris</Link></li>
+     </ul>
+     <FeedSlot/>
+    </Layout>
   )
 }
