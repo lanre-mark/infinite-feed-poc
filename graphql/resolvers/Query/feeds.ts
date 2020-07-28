6fb7477fa730991e1129d47d32c17e704d6b09fc
olanrewaju Makinde <lanre.developer@gmail.com>
2020-07-28 22:25:18 +0100

    fixed resolver postgresql queries

--- graphql/resolvers/Query/feeds.ts
+++ graphql/resolvers/Query/feeds.ts
@@ -18,10 +18,10 @@ const getOrSkipProject = (fellowship: String) => {
 
 export default async function feeds(parent: unknown, {fellowship, limit, skip}: Args): Promise<Row[]> {
   
-    const ANGELS_FOUNDERS = "WHERE u.fellowship IN ('angels', 'founders')";
-    const WRITERS_ONLY = "WHERE u.fellowship = ?";
+    const ANGELS_FOUNDERS = "WHERE fellowship = 'angels' OR fellowship = 'founders'";
+    const WRITERS_ONLY = "WHERE fellowship = 'writers'";
     const USERS_WHERE_CLAUSE = fellowship === "writers"? WRITERS_ONLY:ANGELS_FOUNDERS;
-    const feeds: Row[] = await db.getAll(    `
+    const feeds: Row[] = await db.getAll(`
       SELECT id, Null as name, Null as bio, null as description, null as icon_url, fellowship, 
       title,Null as avatar_url, body, created_ts, updated_ts
       FROM announcements 
@@ -29,9 +29,9 @@ export default async function feeds(parent: unknown, {fellowship, limit, skip}: 
       UNION ALL
       SELECT id,name,bio, Null as description, null as icon_url,fellowship, 
       Null as title, avatar_url, Null as body,created_ts,updated_ts
-      FROM users u ${USERS_WHERE_CLAUSE}        
+      FROM users ${USERS_WHERE_CLAUSE}        
       ${getOrSkipProject(fellowship)}
-      order by updated_ts desc limit ${skip},${limit}
+      order by created_ts desc limit ${skip},${limit}
     `,
     [fellowship]
   )
--- graphql/resolvers/index.ts
+++ graphql/resolvers/index.ts
@@ -1,2 +1,20 @@
-export * as User from './User'
 export * as Project from './Project'
+export * as Query from './Query';
+export * as User from './User'
+export const Feed = {
+    __resolveType: (obj: any, context: any, info: any) => {
+        console.log(typeof obj)
+        console.log('resolving... body:',obj.body,' description: ',obj.description,' bio:', obj.bio)
+
+        if (obj.body) {
+            return 'Announcement';
+        }
+        if (obj.description) {
+            return 'Project'
+        }
+        if (obj.name) {
+            return 'User'
+        }
+        return null;
+    }
+}
